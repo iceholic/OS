657ffa16a73b0a32743d80fbebc7ab5a0fe4ca84
kangheng001 <kangheng001@lianjia.com>
2022-04-26 23:57:35 +0800

    Initial commit

---
+++ ostep/cpu.c
@@ -0,0 +1,24 @@
+//
+// Created by ice on 2022/4/26.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+//#include "common.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
+    }
+
+    char* str = argv[1];
+    while (1) {
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
+
